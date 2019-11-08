fd5e07133261821e1ee64733ef2458628bd51387
Student <student@donation.local>
2019-11-08 15:11:42 -0600

    commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Book(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    published = models.TimeField(datetime.now)
+    genre = models.TextField()
+    in_stock = models.BooleanField()
+    description = models.TextField()
+
+class Transaction(models.Model):
+    datetime = models.TimeField(datetime.now)
+    action = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Book, Transaction
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
+
+
+def borrow_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock:
+        transaction = book.transaction_set.create(False)
+        messages.success(request, "Borrowed <title> by <author>")
+        return redirect("book_list.html", transaction.id)
+    elif book.in_stock:
+        book = Book.objects.get(id=id)
+        transaction = book.transaction_set.create(True)
+        messages.error(request, "<title> by <author> is unavailable")
+        return redirect("book_list.html", transaction.id)
+
+
+def return_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock:
+        transaction = book.transaction_set.create(True)
+        messages.success(request, "returned <title> by <author>")
+        return redirect("transaction_detail.html", transaction.id)
+    elif book.in_stock:
+        book = Book.objects.get(id=id)
+        transaction = book.transaction_set.create(False)
+        messages.error(request, "<title> by <author> is unavailable")
+        return redirect("transaction_detail.html", transaction.id)
